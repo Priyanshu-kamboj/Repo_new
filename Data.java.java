e9462ab8df6fd935725220b38d19435a2015055e
Priyanshu-kamboj <kambojpriyanshu4@gmail.com>
2025-07-23 13:31:25 +0530

    Add files via upload

---
+++ Data.java.java
@@ -0,0 +1,22 @@
+interface Animal{
+    void makeSound();   
+}
+class Dog implements Animal{
+    public void makeSound(){
+        System.out.println("Bark");
+    }
+}
+class Cat implements Animal{
+    public void makeSound(){
+        System.out.println("Meow");
+    }
+}
+public class InterfaceExample {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        
+        dog.makeSound();
+        cat.makeSound(); 
+    }
+}
